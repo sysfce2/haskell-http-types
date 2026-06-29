c57bf86a5c9dff0bca6d7789984763f94794abfd
Felix Paulusma <felix.paulusma@gmail.com>
2026-06-29 17:41:00 +0200

    test: add testing of parsing of status codes

--- test/Network/HTTP/Types/StatusSpec.hs
+++ test/Network/HTTP/Types/StatusSpec.hs
@@ -47,6 +47,25 @@ spec = do
             renderFullStatus (mkStatus 12 "Short") `shouldBe` "012 Short"
             renderFullStatus (mkStatus 987 "Pretty Long If I May Say So Myself")
                 `shouldBe` "987 Pretty Long If I May Say So Myself"
+    describe "Parsing functions" $ do
+        it "parses the code" $ do
+            parseStatusCode "307" `shouldBe` Just (307, "")
+            parseStatusCode "404 Not Found" `shouldBe` Just (404, " Not Found")
+            parseStatusCode "1337 Works Still" `shouldBe` Just (133, "7 Works Still")
+            parseStatusCode "No Digits" `shouldBe` Nothing
+            parseStatusCode "12 Is Not Enough" `shouldBe` Nothing
+        it "parses the message" $ do
+            parseFullStatus "307" `shouldBe` Just (mkStatus 307 "")
+            parseFullStatus "404 Not Found" `shouldBe` Just (mkStatus 404 "Not Found")
+            parseFullStatus "500 Someone Forgot To\r\nBreak At The Newline"
+                `shouldBe` Just (mkStatus 500 "Someone Forgot To\r\nBreak At The Newline")
+            parseFullStatus "1337 Works Still" `shouldBe` Nothing
+            parseFullStatus "No Digits" `shouldBe` Nothing
+        it "round trips" $ do
+            parseFullStatus (renderFullStatus notFound404) `shouldBe` Just notFound404
+            -- I know this is under "round trips" and loses the message, but
+            -- it's about the status code here.
+            parseStatusCode (renderStatusCode notFound404) `shouldBe` Just (404, "")
 
 categoryCheck :: String -> (Status -> Bool) -> [StatusTuple] -> Spec
 categoryCheck name p shoulds = do
